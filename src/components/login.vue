bd53db54ceb052dd4f1fa81844a92964109f0261
Maxwell Knoxx <maxwell.knoxx@live.com>
2019-07-09 18:38:24 +0100

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h1> Oi, eu sou goku </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import login from './components/login'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const routes = [
+
+  {path: '/login', components: login}
+
+]
+
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
